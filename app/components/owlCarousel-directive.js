6943dbbdb34817e1d71bf80c127d3aefd49eafee
Youness HARDI <youness.hardi@gmail.com>
2016-11-04 02:49:25 +0100

    updates (add ng-repeat to carousel)

--- app/components/owlCarousel-directive.js
+++ app/components/owlCarousel-directive.js
@@ -8,10 +8,35 @@
     .directive('wrapOwlcarousel', function () {
       return {
         restrict: 'E',
+        transclude: false,
         link: function (scope, element, attrs) {
-          var options = scope.$eval($(element).attr('data-options'));
-          $(element).owlCarousel(options);
+          scope.initCarousel = function (element) {
+            // provide any default options you want
+            var defaultOptions = {};
+            var customOptions = scope.$eval($(element).attr('data-options'));
+            // combine the two options objects
+            for (var key in customOptions) {
+              defaultOptions[key] = customOptions[key];
+            }
+            // init carousel
+            var curOwl = $(element).data('owlCarousel');
+            if (!angular.isDefined(curOwl)) {
+              $(element).owlCarousel(defaultOptions);
+            }
+            scope.cnt++;
+          };
         }
       };
-    });
+    }).directive('owlCarouselItem', [function () {
+    return {
+      restrict: 'A',
+      transclude: false,
+      link: function (scope, element) {
+        // wait for the last item in the ng-repeat then call init
+        if (scope.$last) {
+          scope.initCarousel(element.parent());
+        }
+      }
+    };
+  }]);
 }());
--- app/home/home-service.js
+++ app/home/home-service.js
@@ -16,13 +16,119 @@
           });
       },
 
-      getLastTrip: function () {
-        return $http.get(EnvironmentConfig.GlobalBaseUrl + "/trip/lastTrip/")
-          .then(function (response) {
-            return response.data;
-          }, function (errResponse) {
-            console.log('Error in AJAX call :' + errResponse);
-          });
+      getPromotions: function () {
+        return [
+          {
+            id: 1,
+            src: "./img/baterie1.png",
+            alt: "",
+            caption: {
+              title: "BATTERIE ACOUSTIQUE ROCK PEARL",
+              details: "758,00 €"
+            }
+          },
+          {
+            id: 2,
+            src: "./img/guitar1.png",
+            alt: "",
+            caption: {
+              title: "EPIPHONE DR-100 EBONY CHROME HARDWARE",
+              details: "129,00 €"
+            }
+          },
+          {
+            id: 3,
+            src: "./img/guitar2.png",
+            alt: "",
+            caption: {
+              title: "EPIPHONE HUMMINGBIRD PRO E-A FADED",
+              details: "289,00 €"
+            }
+          },
+          {
+            id: 4,
+            src: "./img/neumann1.jpg",
+            alt: "",
+            caption: {
+              title: "NEUMANN KH120A",
+              details: "597,00 €"
+            }
+          },
+          {
+            id: 5,
+            src: "./img/pedale1.png",
+            alt: "",
+            caption: {
+              title: "PEDALE LOOPER BOSS RC30",
+              details: "199,00 €"
+            }
+          }
+        ];
+      },
+
+      getBestSales: function () {
+        return [
+          {
+            id: 1,
+            src: {
+              front: "./img/baterie1.png",
+              back: "./img/baterie1.png"
+            },
+            alt: "",
+            caption: {
+              title: "BATTERIE ACOUSTIQUE ROCK PEARL",
+              price: "758,00 €"
+            }
+          },
+          {
+            id: 2,
+            src: {
+              front: "./img/guitar1.png",
+              back: "./img/guitar1.png"
+            },
+            alt: "",
+            caption: {
+              title: "EPIPHONE DR-100 EBONY CHROME HARDWARE",
+              price: "129,00 €"
+            }
+          },
+          {
+            id: 3,
+            src: {
+              front: "./img/guitar2.png",
+              back: "./img/guitar2.png"
+            },
+            alt: "",
+            caption: {
+              title: "EPIPHONE HUMMINGBIRD PRO E-A FADED",
+              price: "289,00 €"
+            }
+          },
+          {
+            id: 4,
+            src: {
+              front: "./img/neumann1.jpg",
+              back: "./img/neumann1.jpg"
+            },
+            alt: "",
+            caption: {
+              title: "NEUMANN KH120A",
+              price: "597,00 €"
+            }
+          },
+          {
+            id: 5,
+            src: {
+              front: "./img/pedale1.png",
+              back: "./img/pedale1.png"
+            },
+            alt: "",
+            caption: {
+              title: "PEDALE LOOPER BOSS RC30",
+              price: "199,00 €"
+            }
+          }
+        ];
       }
 
     }
