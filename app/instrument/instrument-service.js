53e6805c178c1f346352cedd2e66872a669e4b7b
Youness HARDI <youness.hardi@gmail.com>
2016-11-04 04:18:03 +0100

    updates

--- app/instrument/instrument-service.js
+++ app/instrument/instrument-service.js
@@ -15,7 +15,8 @@
           prix: "758,00 €",
           marque:" - ",
           musicien:" - ",
-          categorie: "baterie"
+          categorie: "baterie",
+          description : "Après plusieurs années d'absence, la batterie la plus vendue de l'histoire fait son grand retour. La mythique Pearl Export offre toujours le même rapport qualité/prix et le son qui l'ont rendue célèbre mais cette nouvelle version apporte une multitude de nouveautés dont une pédale Demonator et un pa"
         };
       }
     }
