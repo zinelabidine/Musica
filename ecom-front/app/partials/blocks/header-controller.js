da1e70750dbd881ce24e7335d9cf7986327ef899
HADJ CADI Zine El Abidine <zhadjcadi@gmail.com>
2016-11-11 18:54:48 +0100

    Init front project

---
+++ ecom-front/app/partials/blocks/header-controller.js
@@ -0,0 +1,15 @@
+(function () {
+  "use strict";
+
+  angular.module('app')
+    .controller('HeaderCtrl', ['$scope', function ($scope) {
+
+      var self = this;
+      //$scope.isCollapsed = true;
+      //$scope.$on('$routeChangeSuccess', function () {
+      //  $scope.isCollapsed = true;
+      //});
+
+    }]);
+
+}());
