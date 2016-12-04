254f4ef4f4135252c38b377ca5cf2c7f26a09c8e
Youness HARDI <youness.hardi@gmail.com>
2016-12-04 01:20:05 +0100

    ajout de RolesAllowed et quelques MAJ

--- MusicaServerWSRS/src/main/java/com/ecom/musica/dto/InitializeHeaderDTO.java
+++ MusicaServerWSRS/src/main/java/com/ecom/musica/dto/InitializeHeaderDTO.java
@@ -2,9 +2,20 @@ package com.ecom.musica.dto;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
 import com.ecom.musica.entities.Categorie;
 
+@XmlRootElement
 public class InitializeHeaderDTO {
-	public List<Categorie> Categories;
+	private List<Categorie> Categories;
+
+	public List<Categorie> getCategories() {
+		return Categories;
+	}
+
+	public void setCategories(List<Categorie> categories) {
+		Categories = categories;
+	}
 
 }
--- MusicaServerWSRS/src/main/java/com/ecom/musica/dto/InitializeHomeContentDTO.java
+++ MusicaServerWSRS/src/main/java/com/ecom/musica/dto/InitializeHomeContentDTO.java
@@ -2,10 +2,30 @@ package com.ecom.musica.dto;
 
 import java.util.List;
 
-import com.ecom.musica.entities.Categorie;
+import javax.xml.bind.annotation.XmlRootElement;
+
 import com.ecom.musica.entities.Instrument;
 
+@XmlRootElement
 public class InitializeHomeContentDTO {
-	public List<Instrument> InstrumentsBestSales;
-	public List<Instrument> InstrumentsPromotions;
+
+	private List<Instrument> InstrumentsBestSales;
+	private List<Instrument> InstrumentsPromotions;
+
+	public List<Instrument> getInstrumentsBestSales() {
+		return InstrumentsBestSales;
+	}
+
+	public void setInstrumentsBestSales(List<Instrument> instrumentsBestSales) {
+		InstrumentsBestSales = instrumentsBestSales;
+	}
+
+	public List<Instrument> getInstrumentsPromotions() {
+		return InstrumentsPromotions;
+	}
+
+	public void setInstrumentsPromotions(List<Instrument> instrumentsPromotions) {
+		InstrumentsPromotions = instrumentsPromotions;
+	}
+
 }
