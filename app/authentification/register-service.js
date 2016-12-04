af7d2a98d563361f94bb16b3dbf0b0d28fa2b557
Youness HARDI <youness.hardi@gmail.com>
2016-12-05 00:52:20 +0100

    MAJ de register

--- app/authentification/register-service.js
+++ app/authentification/register-service.js
@@ -10,7 +10,6 @@
         globalService.cleanSessionStorage();
         globalService.login(clientLoginData.login);
         var basicToken = clientLoginData.login + ':' + clientLoginData.mdp;
-        // we support IE10+
         var tokenBase64 = window.btoa(unescape(encodeURIComponent(basicToken)));
 
         $http({
@@ -37,7 +36,6 @@
           .error(function (data, status, headers, config, statusText) {
             globalService.cleanSessionStorage();
             console.log(status);
-            console.log(statusText);
             var failedDetail = {
               "status": status,
               "reason": data.reason
@@ -62,13 +60,44 @@
       }
 
       ,
-      register: function (data) {
-        return $http.post(EnvironmentConfig.GlobalBaseUrl + "/auth/register", data)
-          .then(function (response) {
-            return response.data;
-          }, function (errResponse) {
-            $log.log("Error in AJAX call " + errResponse);
+      register: function (clientRegisterData) {
+
+        var deferred = $q.defer();
+        globalService.cleanSessionStorage();
+        globalService.login(clientRegisterData.login);
+        var basicToken = clientRegisterData.login + ':' + clientRegisterData.mdp;
+        var tokenBase64 = window.btoa(unescape(encodeURIComponent(basicToken)));
+
+        $http({
+          url: EnvironmentConfig.GlobalBaseUrl + "/auth/register",
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json; charset=utf-8'
+          },
+          data: clientRegisterData
+        }).success(function (data, status, headers, config) {
+
+            globalService.token(tokenBase64);
+            // use new Date().getTime()
+            // globalService.expirationDate(data.expirationDate);
+
+            // Store personal data
+            data.userId = clientRegisterData.login;
+            console.log(data);
+            globalService.personalDatas(JSON.stringify(data));
+
+            deferred.resolve(data, status);
           })
+          .error(function (data, status, headers, config, statusText) {
+            globalService.cleanSessionStorage();
+            console.log(status);
+            var failedDetail = {
+              "status": status,
+              "reason": data.reason
+            };
+            return deferred.reject(failedDetail);
+          });
+        return deferred.promise;
       },
       isConnected: function () {
         var accessToken = globalService.token();
