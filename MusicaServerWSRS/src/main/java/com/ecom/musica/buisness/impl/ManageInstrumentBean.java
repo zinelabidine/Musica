caaeca8478d0fee6d402d5f511bde20818326774
Youness HARDI <youness.hardi@gmail.com>
2016-12-12 00:33:05 +0100

    case sensitive get by key

--- MusicaServerWSRS/src/main/java/com/ecom/musica/buisness/impl/ManageInstrumentBean.java
+++ MusicaServerWSRS/src/main/java/com/ecom/musica/buisness/impl/ManageInstrumentBean.java
@@ -63,7 +63,8 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
 
     @Override
     public List<Instrument> getInstrumentsPromotion() {
-        Query req = entityManager.createQuery("select i  from Instrument i inner join i.promotions Left JOIN FETCH i.musiciens mu");
+    	TypedQuery<Instrument> req = entityManager.createQuery("select i  from Instrument i inner join i.promotions "
+    			+ "Left JOIN FETCH i.musiciens mu", Instrument.class);
         List<Instrument> instruments = req.getResultList();
         for (Instrument instrument : instruments) {
             instrument.getPromotions().size();
@@ -82,8 +83,9 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
 
     @Override
     public List<Instrument> getInstrumentsBestSales() {
-        Query req = entityManager.createQuery(
-                "select i,count(i) as NbrVente from Instrument  i Left JOIN FETCH i.musiciens mu inner join i.lignesCommande group by i.instrumentId order by NbrVente desc");
+		Query req = entityManager.createQuery(
+				"select i,count(i) as NbrVente from Instrument  i Left JOIN FETCH i.musiciens mu inner join i.lignesCommande "
+						+ "group by i.instrumentId order by NbrVente desc");
         List<Instrument> instruments = castResultToList(Instrument.class, 0, req.getResultList());
         for (Instrument instrument : instruments) {
             instrument.getPromotions().size();
@@ -107,19 +109,21 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
 
     }
 
-    @Override
-    public List<Instrument> getInstrumentsWithKey(String instrumentKey) {
-    	TypedQuery<Instrument> req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu, Marque m, Categorie c "
-				+ "where (i.reference like :instrumentKey or m.libelle like :instrumentKey "
-				+ "or c.libelle like :instrumentKey) and i.marque = m.marqueId and i.categorie = c.categorieId", Instrument.class);
+	@Override
+	public List<Instrument> getInstrumentsWithKey(String instrumentKey) {
+		TypedQuery<Instrument> req = entityManager.createQuery(
+				"select i from Instrument i Left JOIN FETCH i.musiciens mu, Marque m, Categorie c "
+						+ "where (LOWER(i.reference) like LOWER(:instrumentKey) or LOWER(m.libelle) like LOWER(:instrumentKey) "
+						+ "or LOWER(c.libelle) like LOWER(:instrumentKey)) and i.marque = m.marqueId and i.categorie = c.categorieId",
+				Instrument.class);
 
 		req.setParameter("instrumentKey", "%" + instrumentKey + "%");
-        List<Instrument> instruments = req.getResultList();
-        for (Instrument instrument : instruments) {
-            instrument.getPromotions().size();
-        }
-        return instruments;
-    }
+		List<Instrument> instruments = req.getResultList();
+		for (Instrument instrument : instruments) {
+			instrument.getPromotions().size();
+		}
+		return instruments;
+	}
 
 	@Override
 	public List<Instrument> findInstruments(String marque, String categorie, String instrumentRef) {
