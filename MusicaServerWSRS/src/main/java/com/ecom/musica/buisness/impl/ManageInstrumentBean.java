e2af31d4934addf25db589a2a34a6cb00af990db
Youness HARDI <youness.hardi@gmail.com>
2016-12-11 23:26:21 +0100

    fix lazy loading musiciens

--- MusicaServerWSRS/src/main/java/com/ecom/musica/buisness/impl/ManageInstrumentBean.java
+++ MusicaServerWSRS/src/main/java/com/ecom/musica/buisness/impl/ManageInstrumentBean.java
@@ -7,6 +7,7 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import com.ecom.musica.buisness.ManageInstrumentBeanRemote;
 import com.ecom.musica.entities.Categorie;
@@ -52,7 +53,7 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
 
     @Override
     public List<Instrument> getAllInstruments() {
-        Query req = entityManager.createQuery("select i from Instrument i");
+        Query req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu");
         List<Instrument> instruments = req.getResultList();
         for (Instrument instrument : instruments) {
             instrument.getPromotions().size();
@@ -62,7 +63,7 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
 
     @Override
     public List<Instrument> getInstrumentsPromotion() {
-        Query req = entityManager.createQuery("select i  from Instrument i inner join i.promotions");
+        Query req = entityManager.createQuery("select i  from Instrument i inner join i.promotions Left JOIN FETCH i.musiciens mu");
         List<Instrument> instruments = req.getResultList();
         for (Instrument instrument : instruments) {
             instrument.getPromotions().size();
@@ -82,7 +83,7 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
     @Override
     public List<Instrument> getInstrumentsBestSales() {
         Query req = entityManager.createQuery(
-                "select i,count(i) as NbrVente from Instrument i inner join i.lignesCommande group by i.instrumentId order by NbrVente desc");
+                "select i,count(i) as NbrVente from Instrument  i Left JOIN FETCH i.musiciens mu inner join i.lignesCommande group by i.instrumentId order by NbrVente desc");
         List<Instrument> instruments = castResultToList(Instrument.class, 0, req.getResultList());
         for (Instrument instrument : instruments) {
             instrument.getPromotions().size();
@@ -107,11 +108,12 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
     }
 
     @Override
-    public List<Instrument> getInstrumentsWithKey(String instrument_key) {
-        Query req = entityManager.createQuery("select i" + " from Instrument i, Marque m, Categorie c"
-                + " where (i.reference like '%" + instrument_key + "%'" + " or m.libelle like '%" + instrument_key
-                + "%'" + " or c.libelle like '%" + instrument_key + "%')" + " and i.marque = m.marqueId"
-                + " and i.categorie = c.categorieId");
+    public List<Instrument> getInstrumentsWithKey(String instrumentKey) {
+    	TypedQuery<Instrument> req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu, Marque m, Categorie c "
+				+ "where (i.reference like :instrumentKey or m.libelle like :instrumentKey "
+				+ "or c.libelle like :instrumentKey) and i.marque = m.marqueId and i.categorie = c.categorieId", Instrument.class);
+
+		req.setParameter("instrumentKey", "%" + instrumentKey + "%");
         List<Instrument> instruments = req.getResultList();
         for (Instrument instrument : instruments) {
             instrument.getPromotions().size();
@@ -123,14 +125,13 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
     public List<Instrument> findInstruments(String marque, String categorie, String instrument_ref) {
         Query req = null;
         if (marque.toString().equals("")) {
-            System.out.println("marque null");
             if (instrument_ref.toString().equals("")) {
                 // find with categorie
-                req = entityManager.createQuery("select i " + " from Instrument i ,Categorie c"
+                req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu,Categorie c"
                         + " Where c.libelle like '%" + categorie + "%'" + " and i.categorie = c.categorieId");
             } else {
                 // find with categorie et intrument_ref
-                req = entityManager.createQuery("select i " + " from Instrument i ,Categorie c"
+                req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu,Categorie c"
                         + " Where (i.reference like '%" + instrument_ref + "%'" + " and c.libelle like '%" + categorie
                         + "%')" + " and i.categorie = c.categorieId");
             }
@@ -138,23 +139,23 @@ public class ManageInstrumentBean implements ManageInstrumentBeanRemote {
             if (categorie.toString().equals("")) {
                 if (instrument_ref.toString().equals("")) {
                     // find with marque
-                    req = entityManager.createQuery("select i " + " from Instrument i ,Marque m"
+                    req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu,Marque m"
                             + " Where m.libelle like '%" + marque + "%'" + " and i.marque = m.marqueId");
                 } else {
                     // find with marque et ref
-                    req = entityManager.createQuery("select i " + " from Instrument i ,Marque m"
+                    req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu,Marque m"
                             + " Where (i.reference like '%" + instrument_ref + "%'" + " and m.libelle like '%" + marque
                             + "%')" + " and i.marque = m.marqueId");
                 }
             } else {
                 if (instrument_ref.toString().equals("")) {
                     // find with marque et categorie
-                    req = entityManager.createQuery("select i " + " from Instrument i ,Marque m, Categorie c"
+                    req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu,Marque m, Categorie c"
                             + " Where (c.libelle like '%" + categorie + "%'" + " and m.libelle like '%" + marque + "%')"
                             + " and i.marque = m.marqueId" + " and i.categorie = c.categorieId");
                 } else {
                     // find with 3 criteres
-                    req = entityManager.createQuery("select i" + " from Instrument i, Marque m, Categorie c"
+                    req = entityManager.createQuery("select i from Instrument i Left JOIN FETCH i.musiciens mu, Marque m, Categorie c"
                             + " where (i.reference like '%" + instrument_ref + "%'" + " and m.libelle like '%" + marque
                             + "%'" + " and c.libelle like '%" + categorie + "%')" + " and i.marque = m.marqueId"
                             + " and i.categorie = c.categorieId");
