82c1041246148b8e36af8080064e3bcf9e01dce6
Youness HARDI <youness.hardi@gmail.com>
2016-12-13 00:46:09 +0100

    footer categories redirection

--- app/footer/footer-controller.js
+++ app/footer/footer-controller.js
@@ -2,13 +2,16 @@
   "use strict";
 
   angular.module('app')
-    .controller('FooterCtrl', ['$scope', '$log', '$location', 'ApplicationConfig', 'homeService', function ($scope, $log, $location, ApplicationConfig, homeService) {
+    .controller('FooterCtrl', ['$scope', '$log', '$location', 'ApplicationConfig', 'headerService', function ($scope, $log, $location, ApplicationConfig, headerService) {
 
       var self = this;
-      //$scope.isCollapsed = true;
-      //$scope.$on('$routeChangeSuccess', function () {
-      //  $scope.isCollapsed = true;
-      //});
+
+      $scope.categories = headerService.getCategories();
+
+      $scope.goToCategorie = function (categorieName) {
+        $location.search({});
+        $location.path("/recherche").search('categorieName', categorieName);
+      };
 
     }]);
 }());
