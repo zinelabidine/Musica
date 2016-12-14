9e239448c1b312b3a184bc7181c0d68f58829757
Youness HARDI <youness.hardi@gmail.com>
2016-12-14 12:14:26 +0100

    format et refresh size

--- app/authentification/register-controller.js
+++ app/authentification/register-controller.js
@@ -12,6 +12,7 @@
         registerService.register($scope.clientInfos).then(function (response) {
           // Save informations to user story or to cookies
           $scope.$emit('authentificationEvent', { message: response });
+          $scope.$emit('cartInstrumentChanged', {});
           $location.path('home');
         }, function () {
         })
@@ -20,6 +21,7 @@
       $scope.signin = function () {
         registerService.signin($scope.clientLoginData).then(function (response) {
           $scope.$emit('authentificationEvent', { message: response });
+          $scope.$emit('cartInstrumentChanged', {});
           $location.path('home');
         }, function () {
           $log.error("Je ne peux pas s'authentifier");
