e37b7639544b5d75274ded656dfddbc09637b1e7
Zine El Abidine HADJ CADI <zhadjcadi@gmail.com>
2016-12-17 16:34:09 +0100

    Remove old front project

--- ecom-front/app/app-env.js
+++
@@ -1,6 +0,0 @@
-angular.module('app.env', [])
-  .constant('EnvironmentConfig', {
-    "GlobalBaseUrl": "http://localhost:8000/",
-    "defaultTimeZone": "Europe/paris",
-    "defaultDateFormat": "DD-MM-YYYY HH:mm"
-  });
